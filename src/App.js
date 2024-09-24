6b031ab9e00d2319c2875d8433ebe347da0349d7
Mehddi <beloual.mehdi@yahoo.fr>
2024-09-24 20:30:07 +0200

    Bouton decrement et génération d'un objectif aléatoire

--- src/App.js
+++ src/App.js
@@ -2,21 +2,36 @@ import './App.css';
 import React, { useState } from 'react'; //importation du hook useState
 
 function App() {
-  const [count, setCount] = useState(0); //définition du composant; count : valeur actuelle; setCount : fonction pourmettre à jour la valeur
-  const targetNumber = 10; //Objectif
+  const [count, setCount] = useState(0); // Compteur
+  const [goal, setGoal] = useState(null); // Objectif aléatoire
 
   const increment = () => {
     setCount(count + 1);
-  }
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const generateRandomGoal = () => {
+    const min = 3;
+    const max = 10;
+    // Générer un nombre aléatoire entre 3 et 10
+    const randomAvg = (Math.floor(Math.random() * (max - min) + min)); // Limiter à deux décimales
+    setGoal(Number(randomAvg)); // Met à jour l'objectif en tant que nombre
+  };
+
   return (
     <div className="App">
-      
-      <p>Count: {count}</p>
-      <p>Objectif: {targetNumber}</p>
-      <button onClick={increment}>Increment</button> {/*lol*/}
-      {count === targetNumber && <p>Bravo!</p>}
+      <p>Count: {count}</p> {/* Compteur */}
+      <p>Objectif: {goal}</p> {/* Affichage de l'objectif */}
+      <button onClick={increment}>Increment</button> {/* Bouton d'incrémentation */}
+      <button onClick={decrement}>Decrement</button> {/* Bouton de décrementation */}
+      <button onClick={generateRandomGoal}>Generate Goal</button> {/* Bouton pour générer l'objectif */}
+      {count === goal && <p>Bravo!</p>} {/* Message de victoire si count atteint l'objectif */}
     </div>
   );
 }
 
 export default App;
+
